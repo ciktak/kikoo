17c09e3f537c8a96c6f4c1170987b288c1dc5684
ciktak <53305177+ciktak@users.noreply.github.com>
2020-07-27 16:51:00 +0900

    master

--- tweet/views.py
+++ tweet/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import ListView
 from .models import Post
 
+
 class PostListView(ListView):
-    models = Post
+    model = Post
     template_name = "tweet/index.html"
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    pagenate_by = 5
-    
+    paginate_by = 5
